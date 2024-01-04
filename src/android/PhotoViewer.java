146a5ed0bc4eadff5c60209cb6f45012c5f8dedd
Noah.lou <2419538202@qq.com>
2024-01-04 15:40:53 +0800

    Update PhotoViewer.java

--- src/android/PhotoViewer.java
+++ src/android/PhotoViewer.java
@@ -3,12 +3,16 @@ package com.sarriaroman.PhotoViewer;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class to Open PhotoViewer with the Required Parameters from Cordova
@@ -45,7 +49,18 @@ public class PhotoViewer extends CordovaPlugin {
     }
 
     protected void getPermission() {
-        cordova.requestPermissions(this, REQ_CODE, new String[]{WRITE, READ});
+        List<String> permissions = new ArrayList<String>();
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // Android API 33 and higher
+            permissions.add(Manifest.permission.READ_MEDIA_IMAGES);
+            permissions.add(Manifest.permission.READ_MEDIA_VIDEO);
+        } else {
+            // Android API 32 or lower
+            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        cordova.requestPermissions(this, 0, permissions.toArray(new String[0]));
+        //cordova.requestPermissions(this, REQ_CODE, new String[]{WRITE, READ});
     }
 
     //
